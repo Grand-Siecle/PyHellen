3db5d1d251f8f61e14da7275ea863d84d6511734
rayondemiel <maximehumeau44@gmail.com>
2025-03-12 14:37:37 +0100

    add schemas status models

--- app/routes/service.py
+++ app/routes/service.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from fastapi import APIRouter, Request
 
-from app.schemas.services import HealthCheckResponse
+from app.schemas.services import (HealthCheckResponse,
+                                  StatusResponse, StatusSchema,
+                                  CPUStatusSchema, GPUStatusSchema, ModelStatusSchema)
 from app.schemas.nlp import PieLanguage
-from app.core.utils import check_gpu_availability, get_device, get_n_workers, initialize_tagger
+from app.core.utils import check_gpu_availability, get_device, get_n_workers
 from app.core.settings import Settings
 
 
@@ -27,37 +29,34 @@ async def health_check():
     }
 
 
-@router.get("/api/status")
+@router.get("/api/status", response_model=StatusResponse)
 async def status_models(request: Request):
     """
     Return the status of all taggers and GPU information.
 
     This endpoint checks if each language model is loaded and provides details about GPU availability.
     """
-
-    #TODO maybe cache
     device = get_device()
     status = {}
-    languages = [lang.value for lang in PieLanguage]
 
     # Check GPU status
     gpu_available, gpu_info = check_gpu_availability()
     if gpu_available:
-        status["gpu"] = {
-            "available": gpu_available,
-            "device": gpu_info,
-            "in_use": device == "cuda"
-            }
+        status["gpu"] = GPUStatusSchema(
+            available=True,
+            device=gpu_info,
+            in_use=(device == "cuda")
+        )
     else:
-        status["cpu"] = {
-            "workers": get_n_workers(),
-        }
+        status["cpu"] = CPUStatusSchema(workers=get_n_workers())
 
-    # Check tagger status
-    for language in languages:
-        if language in request.app.state.taggers_ml:
-            status[language] = "loaded"
-        else:
-            status[language] = "not loaded"
+    models = {}
+    for language in PieLanguage:
+        models[language.name] = ModelStatusSchema(
+            language=language.value,
+            status=request.app.state.taggers_ml.get(language.value, "not loaded")
+        )
 
-    return {"status": status}
+    status["models"] = models
+
+    return {"status": StatusSchema(**status)}
--- app/schemas/services.py
+++ app/schemas/services.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from pydantic import BaseModel
-from typing import Optional, Literal
+from pydantic import BaseModel, Field
+from typing import Optional, Literal, Dict, Any
 
 class HealthCheckResponse(BaseModel):
     service_name: str
@@ -8,3 +8,29 @@ class HealthCheckResponse(BaseModel):
     status: Literal["healthy", "error", "maintenance"]
     timestamp: datetime
     details: Optional[dict] = None
+
+class GPUStatusSchema(BaseModel):
+    available: bool
+    device: str
+    in_use: bool
+
+class CPUStatusSchema(BaseModel):
+    workers: int = Field(gt=1)
+
+class ModelStatusSchema(BaseModel):
+    language: str
+    status: Literal["loaded", "loading", "not loaded"]
+
+class StatusSchema(BaseModel):
+    gpu: GPUStatusSchema | None = None
+    cpu: CPUStatusSchema | None = None
+    models: dict[str, ModelStatusSchema] = Field(
+        default=lambda: {
+            "Model1": ModelStatusSchema(language="model1", status="loaded"),
+            "model2": ModelStatusSchema(language="model2", status="not loaded"),
+            "model3": ModelStatusSchema(language="model3", status="loading"),
+        }
+    )
+
+class StatusResponse(BaseModel):
+    status: StatusSchema
